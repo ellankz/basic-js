6f76096ecd4b69ee64ab0206eeb18a72fa7737b4
ellankz <enelipa@gmail.com>
2020-09-29 17:10:52 +0300

    feat: 3 more tasks solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
-const CustomError = require("../extensions/custom-error");
+function genRepeated(inputString, addition, repeterNum, separator) {
+  let outputStr = '';
+  for (let i = 0; i < repeterNum; i++) {
+    outputStr = outputStr + inputString + addition;
+    if (i !== repeterNum - 1) {
+      outputStr = outputStr + separator;
+    }
+  }
+  return outputStr;
+}
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition =
+    options.addition === undefined
+      ? ''
+      : options.addition === null
+      ? 'null'
+      : options.addition.toString();
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  const inputStr = str === null ? 'null' : str.toString();
+
+  const addedStr = genRepeated(
+    addition,
+    '',
+    additionRepeatTimes,
+    additionSeparator
+  );
+
+  return genRepeated(inputStr, addedStr, repeatTimes, separator);
 };
-  
